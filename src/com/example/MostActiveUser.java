8a5aeca6cd02e278d5c755729a9d1f03561be385
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-14 23:17:35 +0530

    Added MostActiveUser

--- src/com/example/MostActiveUser.java
+++ src/com/example/MostActiveUser.java
@@ -39,31 +39,3 @@ public class MostActiveUser {
 
 
 
-
-//            System.out.println(new Vector(m.keySet()).get(0));
-//            System.out.println(new Vector(m.values()).get(0));
-//
-//        Set set = m.entrySet();
-//        set.iterator();
-//
-//        Map.Entry<Integer, Integer> entry = m.entrySet().iterator().next();
-//        int best_count = entry.getKey();
-//        int user_id = entry.getValue();
-
-//        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
-//            System.out.println(entry.getKey() + ":" + entry.getValue());
-//        }
-//
-//        for (User user : userArrayList) {
-//            if (user.getUser_ID() == user_id) {
-//                System.out.println("User with most activity details are as follows : ");
-//                System.out.println("User ID = " + user_id);
-//                System.out.println("User age = " + user.getAge());
-//                System.out.println("User gender = " + user.getGender());
-//                System.out.println("User occupation = " + user.getOccupation());
-//                System.out.println("User's zip code = " + user.getZip_code());
-//                System.out.println("This user has rated movies " + best_count + " times");
-//                break;
-//            }
-//        }
-//
